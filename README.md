7c75bd69734774d4ce7b148bc7b8ccbac70ce6da
Jeongseok Lee <jslee02@gmail.com>
2015-12-03 18:38:21 -0500

    Add fcl 0.3.2

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 | [![Build Status](https://travis-ci.org/jslee02/conan-dart.svg?branch=master)](https://travis-ci.org/jslee02/conan-dart) | [![Build status](https://ci.appveyor.com/api/projects/status/eak0f3hxixra40oq/branch/master?svg=true)](https://ci.appveyor.com/project/jslee02/conan-dart/branch/master) |
 
 ## Packages
-* libccd 2.0
-* eigen 3.2.7
+* eigen 3.2.7 [bictbucket](https://bitbucket.org/eigen/eigen/)
+* libccd 2.0 [github](https://github.com/danfis/libccd)
+* fcl 0.3.2 [github](https://github.com/flexible-collision-library/fcl)
 
---
+++ fcl/0.3/conanfile.py
@@ -0,0 +1,36 @@
+from conans import ConanFile, CMake
+from conans.tools import download, unzip
+import os
+
+class FclConan(ConanFile):
+    name = "fcl"
+    version = "0.3"
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False]}
+    default_options = "shared=True"
+    requires = "libccd/2.0@jslee02/stable"
+    exports = "fcl/*"
+    url="https://github.com/jslee02/conan-dart/tree/master/fcl/0.3"
+
+    def source(self):
+        zip_name = "fcl-0.3.2.zip"
+        download("https://github.com/flexible-collision-library/fcl/archive/0.3.2.zip", zip_name)
+        unzip(zip_name)
+        os.unlink(zip_name)
+
+    def build(self):
+        cmake = CMake(self.settings)
+        shared = "-DBUILD_SHARED_LIBS=1" if self.options.shared else ""
+        self.run('cd fcl-0.3.2 && cmake . %s %s' % (cmake.command_line, shared))
+        self.run("cd fcl-0.3.2 && cmake --build . %s" % cmake.build_config)
+
+    def package(self):
+        self.copy("*.h", dst="include", src="fcl-0.3.2/include")
+        self.copy("*.hxx", dst="include", src="fcl-0.3.2/include")
+        self.copy("*.lib", dst="lib", src="fcl-0.3.2/lib")
+        self.copy("*.a", dst="lib", src="fcl-0.3.2/lib")
+        self.copy("*.so*", dst="lib", src="fcl-0.3.2/lib")
+
+    def package_info(self):
+        self.cpp_info.libs = ["fcl"]
+
