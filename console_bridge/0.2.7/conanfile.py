51ef10b532883fcb765b86465c042438f26590b4
Jeongseok Lee <jslee02@gmail.com>
2015-12-07 02:50:23 -0500

    Install boost from apt-get

--- console_bridge/0.2.7/conanfile.py
+++ console_bridge/0.2.7/conanfile.py
@@ -8,9 +8,14 @@ class ConsoleBridgeConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False]}
     default_options = "shared=True"
-    requires = "Boost/1.59.0@lasote/stable"
+    #requires = "Boost/1.59.0@lasote/stable"
     exports = "console_bridge/*"
     url="https://github.com/jslee02/conan-dart/tree/master/console_bridge/0.2.7"
+
+    def system_requirements(self):
+        self.global_system_requirements=True
+        if self.settings.os == "Linux":
+            self.run("sudo apt-get install libboost-all-dev || true ")
 
     def source(self):
         zip_name = "console_bridge-0.2.7.zip"
